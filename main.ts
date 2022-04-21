aeaf64268ce3824bcc75f17d957420a56c535100
NikoHam <58589799+NikoHam@users.noreply.github.com>
2022-04-21 10:10:13 +0200

    Aktualizovat pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -22,39 +22,39 @@ basic.forever(function () {
     if (getTemperature(TemperatureSensors.LIVING_ROOM) > 30) {
         music.startMelody(music.builtInMelody(Melodies.Nyan), MelodyOptions.Once)
     }
-    if (pins.digitalReadPin(DigitalPin.P3) == 1 && dvere3 == 0) {
+    if (pins.digitalReadPin(DigitalPin.P1) == 1 && dvere3 == 0) {
         radio.sendNumber(31)
         dvere3 = 1
-        pins.digitalWritePin(DigitalPin.P16, 1)
-    } else if (pins.digitalReadPin(DigitalPin.P3) == 1) {
+        pins.servoWritePin(AnalogPin.P13, 180)
+    } else if (pins.digitalReadPin(DigitalPin.P1) == 1) {
         radio.sendNumber(30)
         dvere3 = 0
         pins.servoWritePin(AnalogPin.P0, 0)
-        pins.digitalWritePin(DigitalPin.P16, 0)
+        pins.servoWritePin(AnalogPin.P13, 0)
         pins.digitalWritePin(DigitalPin.P0, 0)
     }
-    if (pins.digitalReadPin(DigitalPin.P3) == 1 && dvere2 == 0) {
+    if (pins.digitalReadPin(DigitalPin.P2) == 1 && dvere2 == 0) {
         radio.sendNumber(20)
         dvere3 = 1
-        pins.digitalWritePin(DigitalPin.P16, 1)
+        pins.servoWritePin(AnalogPin.P14, 180)
         pins.digitalWritePin(DigitalPin.P0, 1)
-    } else if (pins.digitalReadPin(DigitalPin.P3) == 1) {
+    } else if (pins.digitalReadPin(DigitalPin.P2) == 1) {
         radio.sendNumber(21)
         dvere3 = 0
         pins.servoWritePin(AnalogPin.P0, 0)
-        pins.digitalWritePin(DigitalPin.P16, 0)
+        pins.servoWritePin(AnalogPin.P14, 0)
         pins.digitalWritePin(DigitalPin.P0, 0)
     }
     if (pins.digitalReadPin(DigitalPin.P3) == 1 && dvere1 == 0) {
         radio.sendNumber(11)
         dvere3 = 1
-        pins.digitalWritePin(DigitalPin.P16, 1)
+        pins.servoWritePin(AnalogPin.P15, 180)
         basic.pause(2000)
-        pins.digitalWritePin(DigitalPin.P0, 1)
+        pins.digitalWritePin(DigitalPin.P12, 1)
     } else if (pins.digitalReadPin(DigitalPin.P3) == 1) {
         radio.sendNumber(10)
         dvere3 = 0
-        pins.servoWritePin(AnalogPin.P0, 0)
-        pins.digitalWritePin(DigitalPin.P16, 0)
+        pins.servoWritePin(AnalogPin.P15, 0)
+        pins.digitalWritePin(DigitalPin.P12, 0)
     }
 })
